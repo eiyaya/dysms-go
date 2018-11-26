1b7d662288c55ed7f5fc90c0ac610e0bb6931ac5
Char <char@zexabox.com>
2018-11-26 16:06:16 +0800

    简化框架

--- dysmsapi.go
+++ dysmsapi.go
@@ -2,9 +2,15 @@ package dysms
 
 import (
 	"time"
+	// "strings"
 	"errors"
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
 	"net/url"
 	"net/http"
+
+	"github.com/satori/go.uuid"
 	// "crypto/rand"
 )
 
@@ -16,20 +22,30 @@ var (
 )
 
 var (
+	// 接口地址
+	Path string = "http://dysmsapi.aliyuncs.com/"
 	// 用于发送http请求的Client
 	Client = &http.Client{}
 	// 记录所有创建的SmsSender
-	smsSenders = make(map[string]*SmsSender)
+	smsSenders = make(map[string]*smsSender)
+	// 默认的SmsSender
+	Default *smsSender
 )
 
+// 初始化默认 SmsSender
+func init() {
+	Default, _ = &smsSender{}
+	smsSenders["default"] = Default
+}
+
 // 根据给出的名称获取SmsSender
-func GetSmsSender(names ...string) (smsSender *SmsSender) {
+func GetSmsSender(param ...string) (smsSender *smsSender) {
 	// 如果没有参数，返回默认smsSender，
 	// 如果有参数，根据name[0] 返回smsSender
-	if len(names) <=0 {
+	if len(param) <= 0 {
 		return Default
 	} else {
-		name := names[0]
+		name := param[0]
 		// 如果smsSenders中保存了以name为键的SmsSender,
 		// 返回对应的SmsSender,
 		// 如果没有，创建一个新的SmsSender并返回
@@ -41,100 +57,72 @@ func GetSmsSender(names ...string) (smsSender *SmsSender) {
 	}
 }
 
-func NewSmsSender(name string) (smsSender *SmsSender, err error) {
-	// 检查smsSenders 中是否存在以name为键的SmsSender，
-	// 如果已经存在，返回已存在错误
-	smsSender, ok := smsSenders[name]
-	if ok {
-		return nil, ErrAlreadyExisted
-	}
-	// 创建新的SmsSender
-	smsSender = &SmsSender{
-		Format: "json",
-		SigntureMethod: "HMAC-SHA1",
-	}
-	smsSender.GetAccessKeyId = func() string {
-		return smsSender.AccessKeyId
-	}
-	smsSender.GetAccessSecret = func() string {
-		return smsSender.AccessSecret
-	}
-	smsSender.GetTimestamp = func() string {
-		return time.Now().Format("2006-01-02T15:04:05Z")
-	}
-	smsSender.GetSignatureNonce = func() string {
-		// 实现返回随机字符串
-		// TODO: 参考uuidV4
-		return ""
-	}
-	// 以name为键，向smsSenders中添加新创建的SmsSender
-	smsSenders[name] = smsSender
-	return smsSender, nil
-}
 
-type Request struct {
-	// 系统参数
-	Timestamp string
-	Format string
-	SigntureMethod string
-	SignatureVersion string
-	SignatureNonce func() string
-	Signature string
-	// 业务参数
-	Action string
-	Version string
-	RegionId string
-	PhoneNumbers string
-	SignName string
-	TemplateCode string
-	TemplateParam string
-	OutId string
-}
 
 // 实现短信请求的组装和发送
-type SmsSender struct {
+type smsSender struct {
 	// AccessKeyId 和 AccessSecret
 	AccessKeyId string
 	AccessSecret string
+	// 签名
+	SignName string
 	// 模板码
 	TemplateCode string
-	// 格式
-	Format string
-	SigntureMethod string
-
-	// 获取AccessKeyId 和 AccessSecret的函数
-	GetAccessKeyId func() string
-	GetAccessSecret func() string
-	GetTeplateCode func() string
-	GetSignatureNonce func() string
-	// 获取时间戳的函数
-	GetTimestamp func() string
+	// 执行签名的函数
+	SignatureHandler func(values url.Values, accessSecret string) (rstValues url.Values, sign string)
 }
 
-// 根据给出的TemplateParam发送短信
-func (s *SmsSender) Send(templateParam string) (successed bool, err error) {
-	// 检查s.TemplateCode
-	if s.TemplateCode == "" {
-		return false, ErrTemplateCodeNotSet
-	}
-	return s.SendWithTemplate(s.TemplateCode, templateParam)
+func (s *smsSender) timestamp() string {
+	return time.Now().UTC().Format("2006-01-02T15:04:05Z")
 }
 
-// 按照给出的 TemplateCode 和 TemplateParam 发送短信
-func (s *SmsSender) SendWithTemplate(templateCode, templateParam string) (successed bool, err error) {
-	// request := NewRequest()
-	locator, err := url.Parse("dysmsapi.aliyuncs.com")
-	resp, err := Client.Get(locator.String())
-	// 如果请求失败返回失败信息
-	if err != nil {
-		return false, err
-	}
-	// 解析响应
-	err = parseResp(resp)
-	if err != nil {
-		return false, err
-	}
-	return true, nil
+// 根据给出的 phoneNumbers templateParam发送短信
+func (s *smsSender) Send(phoneNumbers, templateParam string) (successed bool, err error) {
+	return RawSend(phoneNumbers, s.SignName, s.TemplateCode, templateParam)
+}
+
+// 按照给出的 所有参数 发送短信
+func RawSend(accessKeyId, accessSecret, phoneNumbers, signName, templateCode, templateParam string) (requestId, code, message, bizId string) {
+	values := url.Values{}
+	// 权限ID
+	values.Add("AccessKeyId", accessKeyId)
+	// 电话号码
+	values.Add("PhoneNumbers", phoneNumbers)
+	// 签名
+	values.Add("SignName", signName)
+	// 模板码
+	values.Add("TemplateCode", templateCode)
+	// 模板数据
+	values.Add("TemplateParam", templateParam)
+	// 时间戳
+	values.Add("Timestamp", time.Now().UTC().Format("2006-01-02T15:04:05Z"))
+	// 行为
+	values.Add("Action", "SendSms")
+	values.Add("Version", "2017-05-25")
+	values.Add("RegionId", "cn-hangzhou")
+	signedArgs = Signature(values)
+	// 发送请求
+	resp, err := Client.Get(Path+"?"+signedArgs)
+	// 处理结果
+	return parseResp(resp)
+}
+
+// 使用给出的 accessSecret 进行签名
+func Signature(values url.Values, accessSecret string) (signedArgs string) {
+	// 签名方法
+	values.Add("SignatureMethod", "HMAC-SHA1")
+	// 签名版本
+	values.Add("SignatureVersion", "1.0")
+	// 随机数
+	values.Add("SignatureNonce", uuid.Must(uuid.NewV4()).String())
+	// 去除签名字段
+	values.Del("Signature")
+	urlEncoded := values.Encode()
+	stringToSign := "GET"+"&"+specialUrlEncode("/")+"&"+specialUrlEncode(urlEncoded)
+	h := hmac.New(sha1.New, []byte(accessSecret+"&"))
+	h.Write([]byte(stringToSign))
+	signString := base64.StdEncoding.EncodeToString(h.Sum(nil))
+	return values, signString
 }
 
 // 解析返回值
--- url.go
+++ url.go
@@ -2,11 +2,12 @@ package dysms
 
 import (
 	"strings"
-	"net/url"
+	// "net/url"
 )
 
 func specialUrlEncode(value string) string {
-	rstValue := url.QueryEscape(value)
+	// rstValue := url.QueryEscape(value)
+	rstValue := "GET&/&"+value
 	rstValue = strings.Replace(rstValue, "+", "%20", -1)
 	rstValue = strings.Replace(rstValue, "*", "%2A", -1)
 	rstValue = strings.Replace(rstValue, "%7E", "~", -1)
